7c211566b0206667a3dac1253468f21986f6cc30
MIfiner <micserficser@gmail.com>
2026-07-06 19:49:49 +0200

    Create MifMod.java

---
+++ src/main/java/com/yourname/mifmoder/MifMod.java
@@ -0,0 +1,1 @@
+package com.yourname.mifmoder; import net.fabricmc.api.ModInitializer; import org.slf4j.Logger; import org.slf4j.LoggerFactory; public class MifMod implements ModInitializer { public static final String MOD_ID = "mifmoder"; public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID); @Override public void onInitialize() { LOGGER.info("MifMod initialized!"); } }
